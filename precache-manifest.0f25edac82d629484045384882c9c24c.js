2aac59fc4d2659a25c57de2f0a03d098a1885572
arayhan <rayhan@badr.co.id>
2020-01-18 11:15:13 +0700

    Updates

---
+++ precache-manifest.0f25edac82d629484045384882c9c24c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17ce2cc4575d2a950b8bafc0d902983",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "3c2c6abd1e58acaace0e",
+    "url": "/react-gh-pages/static/js/2.e627753e.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/react-gh-pages/static/js/2.e627753e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb990f2456773acadb2e",
+    "url": "/react-gh-pages/static/js/main.a7028f3a.chunk.js"
+  },
+  {
+    "revision": "2869f17bc23bd38e2d31",
+    "url": "/react-gh-pages/static/js/runtime-main.9338059d.js"
+  }
+]);
