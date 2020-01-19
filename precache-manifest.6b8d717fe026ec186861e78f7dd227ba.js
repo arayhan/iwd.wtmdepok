7ed425b4ef73cc2b5256718b5d90bc1d0a1ec6ff
arayhan <rayhan@badr.co.id>
2020-01-19 18:22:32 +0700

    Updates

---
+++ precache-manifest.6b8d717fe026ec186861e78f7dd227ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4876661923033dcd85135abd0c3cb5",
+    "url": "//arayhan.github.io/iwd.wtmdepok/index.html"
+  },
+  {
+    "revision": "1282e3a079f776f005b3",
+    "url": "//arayhan.github.io/iwd.wtmdepok/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d0d4483cde426bf650fd",
+    "url": "//arayhan.github.io/iwd.wtmdepok/static/js/2.38fbd08b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "//arayhan.github.io/iwd.wtmdepok/static/js/2.38fbd08b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1282e3a079f776f005b3",
+    "url": "//arayhan.github.io/iwd.wtmdepok/static/js/main.f3b2bc3c.chunk.js"
+  },
+  {
+    "revision": "b06e25d302ec997ac1c3",
+    "url": "//arayhan.github.io/iwd.wtmdepok/static/js/runtime-main.f2a5f561.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "//arayhan.github.io/iwd.wtmdepok/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2922551568ff57a9ac5c1a93a78e5275.js"
+  "//arayhan.github.io/iwd.wtmdepok/precache-manifest.6b8d717fe026ec186861e78f7dd227ba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//arayhan.github.io/iwd.wtmdepok/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
