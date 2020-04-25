77d4b06fd56ae63eef86d7650532a9e2f2d671b7
arayhan <rayhanprima99@gmail.com>
2020-04-25 15:58:51 +0700

    speakers - done

--- src/pages/Speakers/Speakers.js
+++ src/pages/Speakers/Speakers.js
@@ -10,6 +10,52 @@ class Speakers extends Component {
 				{
 					name: 'Surya Firdaus',
 					photo: SuryaFirdaus,
+					description: 'Frontend Developer at Buka Lapak',
+					social_media: {
+						facebook: 'https://bit.ly/CRA-PWA',
+						twitter: 'https://twitter.com'
+					}
+				},
+				{
+					name: 'Prasetya Dwi Cahya',
+					photo: 'https://devfest-depok.firebaseapp.com/images/speakers/prasetya-dwi-cahya.jpg',
+					description: 'Head of Data Science Indonesia',
+					social_media: {
+						facebook: 'https://bit.ly/CRA-PWA',
+						twitter: 'https://twitter.com'
+					}
+				},
+				{
+					name: 'Yong Shean',
+					photo: 'https://devfest-depok.firebaseapp.com/images/speakers/yong-shean.jpeg',
+					description: 'Frontend Developer, Commerce Solutions',
+					social_media: {
+						facebook: 'https://bit.ly/CRA-PWA',
+						twitter: 'https://twitter.com'
+					}
+				},
+				{
+					name: 'Prasetya Dwi Cahya',
+					photo: 'https://devfest-depok.firebaseapp.com/images/speakers/prasetya-dwi-cahya.jpg',
+					description: 'Head of Data Science Indonesia',
+					social_media: {
+						facebook: 'https://bit.ly/CRA-PWA',
+						twitter: 'https://twitter.com'
+					}
+				},
+				{
+					name: 'Muh. Rahmatullah',
+					photo: 'https://devfest-depok.firebaseapp.com/images/speakers/muh-rahmatullah.jpg',
+					description: 'Software Engineer - Android, Grab',
+					social_media: {
+						facebook: 'https://bit.ly/CRA-PWA',
+						twitter: 'https://twitter.com'
+					}
+				},
+				{
+					name: 'Yong Shean',
+					photo: 'https://devfest-depok.firebaseapp.com/images/speakers/yong-shean.jpeg',
+					description: 'Frontend Developer, Commerce Solutions',
 					social_media: {
 						facebook: 'https://bit.ly/CRA-PWA',
 						twitter: 'https://twitter.com'
@@ -18,6 +64,7 @@ class Speakers extends Component {
 				{
 					name: 'Surya Firdaus',
 					photo: SuryaFirdaus,
+					description: 'Frontend Developer at Buka Lapak',
 					social_media: {
 						facebook: 'https://bit.ly/CRA-PWA',
 						twitter: 'https://twitter.com'
@@ -32,20 +79,25 @@ class Speakers extends Component {
 			<div>
 				<HeaderPage title="Speakers" subtitle="Our lineup of big thinkers and even bigger doers." />
 				<div className="container py-60">
-					<div className="row">
+					<div className="row-wrap justify-center">
 						{this.state.speakers.map(speaker => (
-							<div>
-								<img src={speaker.photo} alt={speaker.name} />
-								<div className="font-size-normal">Surya Firdaus</div>
-								<div className="row">
-									<a href={speaker.social_media.facebook} target="blank">
+							<div className="row col align-center justify-between w-min-220 w-max-220 m-20 p-20 border-1-grey">
+								<div className="row col align-center mb-20">
+									<div className="img-circle w-150 h-150 mb-10">
+										<img src={speaker.photo} alt={speaker.name} />
+									</div>
+									<div className="font-size-large color-font-text-title font-bold font-center pb-10">{speaker.name}</div>
+									<div className="font-size-small color-font-text-secondary font-center">{speaker.description}</div>
+								</div>
+								<div className="row justify-center">
+									<a className="color-font-text-primary px-5 mx-10" href={speaker.social_media.facebook} target="blank">
 										<i class="fab fa-facebook-f"></i>
 									</a>
-									<a href={speaker.social_media.facebook} target="blank">
+									<a className="color-font-text-primary px-5 mx-10" href={speaker.social_media.facebook} target="blank">
 										<i class="fab fa-instagram"></i>
 									</a>
-									<a href={speaker.social_media.facebook} target="blank">
-										<i class="fab fa-meetup"></i>
+									<a className="color-font-text-primary px-5 mx-10" href={speaker.social_media.facebook} target="blank">
+										<i class="fab fa-linkedin"></i>
 									</a>
 								</div>
 							</div>
