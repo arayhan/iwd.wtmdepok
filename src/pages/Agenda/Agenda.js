f8b8f7bde440691203e7b77c78b10b5cb4c49d16
arayhan <rayhanprima99@gmail.com>
2020-04-25 14:29:12 +0700

    update agenda

--- src/pages/Agenda/Agenda.js
+++ src/pages/Agenda/Agenda.js
@@ -28,15 +28,99 @@ class Agenda extends Component {
 					</div>
 				</div>
 
-				<div className="container py-60">
-					{this.state.active_tab === 'day-1' &&
-						<div>Day 1</div>
-					}
+				{this.state.active_tab === 'day-1' &&
+					<div className="list container py-60">
+						<div className="list-item row pb-40">
+							<div className="font-size-normal pr-20">08:00</div>
+							<div>
+								<div className="font-size-normal font-bold pb-10">Start Exploring TensorFlow.js</div>
+								<em className="font-size-normal">Alamanda Shantika</em>
+							</div>
+						</div>
+						<div className="list-item row pb-40">
+							<div className="font-size-normal pr-20">08:00</div>
+							<div>
+								<div className="font-size-normal font-bold pb-10">Start Exploring TensorFlow.js</div>
+								<em className="font-size-normal">Alamanda Shantika</em>
+							</div>
+						</div>
+						<div className="list-item row pb-40">
+							<div className="font-size-normal pr-20">08:00</div>
+							<div>
+								<div className="font-size-normal font-bold pb-10">Start Exploring TensorFlow.js</div>
+								<em className="font-size-normal">Alamanda Shantika</em>
+							</div>
+						</div>
+						<div className="list-item row pb-40">
+							<div className="font-size-normal pr-20">08:00</div>
+							<div>
+								<div className="font-size-normal font-bold pb-10">Start Exploring TensorFlow.js</div>
+								<em className="font-size-normal">Alamanda Shantika</em>
+							</div>
+						</div>
+						<div className="list-item row pb-40">
+							<div className="font-size-normal pr-20">08:00</div>
+							<div>
+								<div className="font-size-normal font-bold pb-10">Start Exploring TensorFlow.js</div>
+								<em className="font-size-normal">Alamanda Shantika</em>
+							</div>
+						</div>
+						<div className="list-item row pb-40">
+							<div className="font-size-normal pr-20">08:00</div>
+							<div>
+								<div className="font-size-normal font-bold pb-10">Start Exploring TensorFlow.js</div>
+								<em className="font-size-normal">Alamanda Shantika</em>
+							</div>
+						</div>
+					</div>
+				}
 
-					{this.state.active_tab === 'day-2' &&
-						<div>Day 2</div>
-					}
-				</div>
+				{this.state.active_tab === 'day-2' &&
+					<div className="list container py-60">
+						<div className="list-item row pb-40">
+							<div className="font-size-normal pr-20">09:00</div>
+							<div>
+								<div className="font-size-normal font-bold pb-10">Jetpack Compose: Modern UI Framework in Android</div>
+								<em className="font-size-normal">Muh. Rahmatullah</em>
+							</div>
+						</div>
+						<div className="list-item row pb-40">
+							<div className="font-size-normal pr-20">08:00</div>
+							<div>
+								<div className="font-size-normal font-bold pb-10">Jetpack Compose: Modern UI Framework in Android</div>
+								<em className="font-size-normal">Muh. Rahmatullah</em>
+							</div>
+						</div>
+						<div className="list-item row pb-40">
+							<div className="font-size-normal pr-20">08:00</div>
+							<div>
+								<div className="font-size-normal font-bold pb-10">Jetpack Compose: Modern UI Framework in Android</div>
+								<em className="font-size-normal">Muh. Rahmatullah</em>
+							</div>
+						</div>
+						<div className="list-item row pb-40">
+							<div className="font-size-normal pr-20">08:00</div>
+							<div>
+								<div className="font-size-normal font-bold pb-10">Jetpack Compose: Modern UI Framework in Android</div>
+								<em className="font-size-normal">Muh. Rahmatullah</em>
+							</div>
+						</div>
+						<div className="list-item row pb-40">
+							<div className="font-size-normal pr-20">08:00</div>
+							<div>
+								<div className="font-size-normal font-bold pb-10">Jetpack Compose: Modern UI Framework in Android</div>
+								<em className="font-size-normal">Muh. Rahmatullah</em>
+							</div>
+						</div>
+						<div className="list-item row pb-40">
+							<div className="font-size-normal pr-20">08:00</div>
+							<div>
+								<div className="font-size-normal font-bold pb-10">Jetpack Compose: Modern UI Framework in Android</div>
+								<em className="font-size-normal">Muh. Rahmatullah</em>
+							</div>
+						</div>
+					</div>
+				}
 			</div>
 		);
 	}
