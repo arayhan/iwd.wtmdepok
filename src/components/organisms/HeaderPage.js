1c086d792a350777864934992fafab9a716fdfb0
arayhan <rayhanprima99@gmail.com>
2020-04-25 15:23:16 +0700

    fix header page background & navbar style

--- src/components/organisms/HeaderPage.js
+++ src/components/organisms/HeaderPage.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
+import Background from "../../assets/images/pictures/dashboard-banner.png"
 
 class HeaderPage extends Component {
 	render() {
 		const { title, subtitle } = this.props
 
 		return (
-			<header>
+			<header style={{
+				background: `url('${Background}') center center no-repeat`,
+				backgroundSize: 'cover',
+				zIndex: '-1'
+			}}>
 				<div div className="container" >
-					<div className="py-60">
+					<div className="py-100">
 						<div className="font-bold font-size-title-small color-font-text-primary">{title}</div>
 						<div className="font-size-large color-font-text-primary pt-15">{subtitle}</div>
 					</div>
