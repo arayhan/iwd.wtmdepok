397036599009aa9254ec18821e8192032423c7f9
arayhan <rayhanprima99@gmail.com>
2020-04-26 22:02:34 +0700

    update agenda

--- src/pages/Agenda/Agenda.js
+++ src/pages/Agenda/Agenda.js
@@ -9,68 +9,156 @@ class Agenda extends Component {
 			agenda: [
 				{
 					id: 'day-1',
-					date: 'March, 21st',
+					date: 'Saturday, May 9th',
 					topics: [
 						{
-							time: '08:00',
-							title: 'Start Exploring TensorFlow.js',
-							speaker: 'Alamanda Shantika'
+							time: '13:40',
+							title: 'Preparation',
 						},
 						{
-							time: '09:00',
-							title: 'Start Exploring TensorFlow.js',
-							speaker: 'Muh. Rahmatullah'
+							time: '14:00',
+							title: 'Welcoming from WTM Ambassadors',
 						},
 						{
-							time: '09:00',
-							title: 'Start Exploring TensorFlow.js',
-							speaker: 'Muh. Rahmatullah'
+							time: '14:10',
+							title: 'Keynote: Self Promotion - Know Your Worth and Ask for It',
+							speaker: 'Prelo Indonesia - Fransiska P.W. Hadiwidjana'
 						},
 						{
-							time: '09:00',
-							title: 'Start Exploring TensorFlow.js',
-							speaker: 'Muh. Rahmatullah'
+							time: '14:40',
+							title: 'Break',
 						},
 						{
-							time: '09:00',
-							title: 'Start Exploring TensorFlow.js',
-							speaker: 'Muh. Rahmatullah'
+							time: '14:50',
+							title: 'Tech Talk: Data - GCP Big Query',
+							speaker: 'OVO - Florentin Anggraini Purnama'
 						},
 						{
-							time: '09:00',
-							title: 'Start Exploring TensorFlow.js',
-							speaker: 'Muh. Rahmatullah'
+							time: '15:20',
+							title: 'Closing',
 						},
 					]
 				},
 				{
 					id: 'day-2',
-					date: 'March, 22st',
+					date: 'Sunday, May 10th',
 					topics: [
 						{
-							time: '09:00',
-							title: 'Jetpack Compose: Modern UI Framework in Android',
-							speaker: 'Muh. Rahmatullah'
+							time: '13:40',
+							title: 'Preparation',
 						},
 						{
-							time: '09:00',
-							title: 'Jetpack Compose: Modern UI Framework in Android',
-							speaker: 'Muh. Rahmatullah'
+							time: '14:00',
+							title: 'Day 1 Recap',
 						},
 						{
-							time: '09:00',
-							title: 'Jetpack Compose: Modern UI Framework in Android',
-							speaker: 'Muh. Rahmatullah'
+							time: '14:10',
+							title: 'Keynote : Mental Wellness - Fighting Impostor Syndrome',
+							speaker: 'Lady Noor Chita Mawardi (Co-founder IBUNDA.ID)'
 						},
 						{
-							time: '09:00',
-							title: 'Jetpack Compose: Modern UI Framework in Android',
-							speaker: 'Muh. Rahmatullah'
+							time: '14:40',
+							title: 'Break',
 						},
 						{
-							time: '09:00',
-							title: 'Jetpack Compose: Modern UI Framework in Android',
-							speaker: 'Muh. Rahmatullah'
+							time: '14:50',
+							title: 'Tech Talk: Data - Data Driven vs Data Centric',
+							speaker: 'Bukalapak, Fatia Kusuma Dewi'
+						},
+						{
+							time: '15:20',
+							title: 'Break',
+						},
+						{
+							time: '15:30',
+							title: 'Tech Talk: Product - Agile Product Management',
+							speaker: 'Traveloka, Tania Soerianto'
+						},
+						{
+							time: '16:00',
+							title: 'Closing',
+						},
+					]
+				},
+				{
+					id: 'day-3',
+					date: 'Saturday, May 16th',
+					topics: [
+						{
+							time: '13:40',
+							title: 'Preparation',
+						},
+						{
+							time: '14:00',
+							title: 'Day 2 Recap',
+						},
+						{
+							time: '14:10',
+							title: 'Keynote : Community Contribution - Pay it Forward by Contributing to Community',
+							speaker: 'Gojek - Nadine Siregar'
+						},
+						{
+							time: '14:40',
+							title: 'Break',
+						},
+						{
+							time: '14:50',
+							title: 'Male Allies - Women Empowerment Through Technology',
+							speaker: 'Halosis - Andrew Darmadi'
+						},
+						{
+							time: '15:20',
+							title: 'Break',
+						},
+						{
+							time: '15:30',
+							title: 'Tech Talk: Data - Using Google Analytics to make your instagram more engaging',
+							speaker: 'Pintek - Siti Nurlaelasari'
+						},
+						{
+							time: '16:00',
+							title: 'Closing',
+						},
+					]
+				},
+				{
+					id: 'day-4',
+					date: 'Sunday, May 17th',
+					topics: [
+						{
+							time: '13:40',
+							title: 'Preparation',
+						},
+						{
+							time: '14:00',
+							title: 'Day 3 Recap',
+						},
+						{
+							time: '14:10',
+							title: 'Keynote : Self Confidence - The Truth: Self-Confidence Isn\'t Only About You',
+							speaker: 'Pintek - Deany Jaghdour'
+						},
+						{
+							time: '14:40',
+							title: 'Break',
+						},
+						{
+							time: '14:50',
+							title: 'Tech Talk : TensorFlow - A Whirlwind Tour of Machine Learning with TensorFlow',
+							speaker: 'Gojek, Galuh Sahid'
+						},
+						{
+							time: '15:20',
+							title: 'Break',
+						},
+						{
+							time: '15:30',
+							title: 'Tech Talk : Product',
+							speaker: 'Stoqo - Vina Zerlina'
+						},
+						{
+							time: '16:00',
+							title: 'Speed mentoring, mock interview',
 						},
 					]
 				},
@@ -112,8 +200,8 @@ class Agenda extends Component {
 					}
 				})}
 			</div>
-		);
+		)
 	}
 }
 
-export default Agenda;
+export default Agenda
